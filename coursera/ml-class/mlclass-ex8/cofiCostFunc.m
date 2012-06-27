40d9410230743f7122bce78acbc248d47f2e3951
Michael Liu <michael.liudl@gmail.com>
2012-06-27 18:35:51 +0800

    finish ex8

--- coursera/ml-class/mlclass-ex8/cofiCostFunc.m
+++ coursera/ml-class/mlclass-ex8/cofiCostFunc.m
@@ -39,10 +39,15 @@ Theta_grad = zeros(size(Theta));
 %        Theta_grad - num_users x num_features matrix, containing the 
 %                     partial derivatives w.r.t. to each element of Theta
 %
+J = sum(sum(((X * Theta' - Y) .* R) .^ 2)) / 2;
 
+X_grad = ((X * Theta' - Y) .* R) * Theta;
+Theta_grad = ((X * Theta' - Y) .* R)' * X;
 
-
-
+% add regularization
+J = J + (sum(sum(Theta .^ 2)) + sum(sum(X .^ 2))) * lambda / 2;
+X_grad = X_grad + lambda * X;
+Theta_grad = Theta_grad + lambda * Theta;
 
 
 
--- coursera/ml-class/mlclass-ex8/selectThreshold.m
+++ coursera/ml-class/mlclass-ex8/selectThreshold.m
@@ -22,11 +22,13 @@ for epsilon = min(pval):stepsize:max(pval)
     %               
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
-
-
-
-
-
+    pred_bin = pval < epsilon;
+    tp = sum(pred_bin == 1 & yval == 1);
+    fp = sum(pred_bin == 1 & yval == 0);
+    fn = sum(pred_bin == 0 & yval == 1);
+    precision = tp / (tp + fp);
+    recall = tp / (tp + fn);
+    F1 = 2 * precision * recall / (precision + recall);
 
 
 
